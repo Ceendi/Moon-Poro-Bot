7857e577c8d6d1ac3ffebe5e1a5f8bad1f3045db
Ceendi <ceendek@gmail.com>
2022-07-26 00:38:14 +0200

    delete timeout on everyone ping

--- main.py
+++ main.py
@@ -27,44 +27,6 @@ class Bot(commands.Bot):
         print(f"Zalogowano jako {self.user}!")
 
     async def on_message(self, message: discord.Message):
-        if '@everyone' in message.content.lower() and "Administracja" not in str(message.author.roles) and "Moderacja" not in str(message.author.roles):
-            data = await self.pool.fetch("SELECT * FROM warn WHERE id=$1;", message.author.id)
-            timeout = discord.utils.get(message.guild.roles, name="TIMEOUT")
-            now = datetime.datetime.utcnow().replace(microsecond=0)
-            enddate = now + datetime.timedelta(days=5)
-            warn_channel = message.guild.get_channel(config.warn_channel_id)
-            if not data:
-                embed = discord.Embed(title=str(timeout), description="2" + " punkt regulaminu", colour=discord.Colour.red())
-                embed.add_field(name="Data otrzymania", value="<t:"+str(now.timestamp())[:-2]+":F>")
-                embed.add_field(name="Data zakończenia", value="<t:"+str(enddate.timestamp())[:-2]+":F>")
-                embed.add_field(name="Użytkownik", value=message.author.mention, inline=False)
-                embed.add_field(name="Mod", value="<@"+str(self.user.id)+">")
-                mes = await warn_channel.send(embed=embed)
-                await self.pool.execute("INSERT INTO warn VALUES($1, $2, $3, $4, $5, $6, $7, $8);", message.author.id, 3, '2', now, enddate, mes.id, [self.user.id], True)
-                await message.author.add_roles(timeout)
-                await message.channel.send(message.author.mention + " otrzymał **" + str(timeout) + "** za 2 punkt regulaminu.")
-            else:
-                data = data[0]
-                old_warn_role = discord.utils.get(message.guild.roles, name=config.warns[data['typ']])
-                powod = data['powod'] + '/2'
-                autorzy = data['autorzy'] + [self.user.id]
-                embed = discord.Embed(title=str(timeout), description=powod + " punkt regulaminu", colour=discord.Colour.red())
-                embed.add_field(name="Data otrzymania", value="<t:"+str(data['start'].timestamp())[:-2]+":F>")
-                embed.add_field(name="Data zakończenia", value="<t:"+str(enddate.timestamp())[:-2]+":F>")
-                embed.add_field(name="Użytkownik", value=message.author.mention, inline=False)
-                for autor in autorzy:
-                    embed.add_field(name="Mod", value="<@"+str(autor)+">")
-                mes = warn_channel.get_partial_message(data['message_id'])
-                await mes.edit(embed=embed)
-                async with self.pool.acquire() as con:
-                    await con.execute("DELETE FROM warn WHERE id=$1 AND active=$2;", message.author.id, False)
-                    await con.execute('UPDATE warn SET active=$1 WHERE id=$2;', False, message.author.id)
-                    await con.execute("INSERT INTO warn VALUES($1, $2, $3, $4, $5, $6, $7, $8);", message.author.id, 3, powod, data['start'], enddate, mes.id, autorzy, True)
-                await message.author.add_roles(timeout)
-                await message.author.remove_roles(old_warn_role)
-                await message.channel.send(message.author.mention + " otrzymał **" + str(timeout) + "** za 2 punkt regulaminu.")
-
-                
         if "buu" in message.content.lower():
             await message.channel.send("Waaa")
         if "jd" in message.content.lower():
