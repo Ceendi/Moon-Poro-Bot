91ed243656c3bc47cf9134a6f30f4b3fc9e026b5
247637 Cendalski Jakub <247637@edu.p.lodz.pl>
2024-01-04 19:15:21 +0100

    check bug

--- cogs/warn.py
+++ cogs/warn.py
@@ -26,12 +26,7 @@ class Warn(commands.Cog):
     async def czysc_warny(self):
         guild = self.bot.get_guild(config.guild_id)
         await guild.chunk(cache=True)
-        print(guild.chunked)
         channel = guild.get_channel(config.warn_channel_id)
-        print(self.bot.guilds)
-        print(guild.id)
-        print(guild.channels)
-        print(channel.id)
         async with self.bot.pool.acquire() as con:
             datas = await con.fetch("SELECT * FROM warn WHERE NOW() > koniec AND active=$1;", True)
             for data in datas:
